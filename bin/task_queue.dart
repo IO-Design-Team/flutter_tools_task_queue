27ce17f221f0b5537a9b68e00cb19ab2d44e9a1b
Rexios <rexios@rexios.dev>
2023-04-26 12:50:42 -0400

    Initial commit

---
+++ bin/task_queue.dart
@@ -0,0 +1,5 @@
+import 'package:task_queue/task_queue.dart' as task_queue;
+
+void main(List<String> arguments) {
+  print('Hello world: ${task_queue.calculate()}!');
+}
---
+++ test/task_queue_test.dart
@@ -0,0 +1,8 @@
+import 'package:task_queue/task_queue.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
